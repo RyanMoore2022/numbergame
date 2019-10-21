f3bf9e7ba2e2bfd12bcfb647d575e84ba5bb0e88
Ryan Moore <ryan.moore.2022@student.lhschools.org>
2019-10-21 10:01:17 -0400

    started fixing loop issue

--- src/numberGame.java
+++ src/numberGame.java
@@ -3,78 +3,44 @@ import java.util.Scanner;
 public class numberGame {
     public static void main(String[] args) {
 
-        System.out.println("Hello, welcome to the number guessing game. You must find the correct number between 1 and 100.");
         int pickNumber;
+        int guessedNumber;
+        String guess;
+
+        System.out.println("Hello, welcome to the number guessing game. You must find the correct number between 1 and 100.");
         pickNumber = (int) (Math.random() * 100) + 1;
         Scanner keyboard = new Scanner(System.in);
         System.out.println(pickNumber);
-        int guessedNumber;
 
-
-        //game loop
-        do{
-
-
-
-            String answer;
-            System.out.println("Do you want to play again?");
-            answer = keyboard.nextLine();
-
-
-            if (answer.equalsIgnoreCase("yes")) {
-                answer = "yes";
-            }
-            else { answer.equals("no"){
-            }
-        }while(answer != "yes");
-
-/*
-
-            //logic
-            do {
-
+        do {
                 guessedNumber = keyboard.nextInt();
 
-
-                if (guessedNumber < pickNumber) {
-                    System.out.println("Too low, guess again.");
-
-                } else if (guessedNumber > pickNumber) {
-                    System.out.println("Too high, guess again.");
-
-                } else {
-                    System.out.println("Congratulations");
-                    System.out.println("Do you want to play again?");
-                    keyboard.nextInt();
+            if (guessedNumber < pickNumber) {
+                System.out.println("Too low, guess again.");
+                guess = "wrong";
 
                 }
 
+                else if (guessedNumber > pickNumber) {
+                System.out.println("Too high, guess again.");
+                guess = "wrong";
 
 
-            } while (guessedNumber != pickNumber);
+            } else {
+                System.out.println("congradulations");
+                guess = "right";
 
-
-*/
-
-
-
-
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            }
+            String answer;
+            System.out.println("Do you want to play again?");
+            answer = keyboard.nextLine();
+                do {
+                    if (answer.equalsIgnoreCase("yes")) {
+                        answer = "yes";
+                    } else {
+                        answer.equals("no");
+                    }
+                }while (guess.equals("wrong"));
+        }while (answer != "yes");
+    }
 }
