867b5c9068a4b60b681cf9920308477d18dc3da7
Ryan Moore <ryan.moore.2022@student.lhschools.org>
2019-10-24 10:27:21 -0400

    added method

--- src/numberGame.java
+++ src/numberGame.java
@@ -2,40 +2,52 @@ import java.util.Scanner;
 
 public class numberGame {
     public static void main(String[] args) {
+
+       //initialized
+        int guessedNumber;
+        int pickNumber;
         boolean guess;
         String answer;
-        int pickNumber;
-        int guessedNumber;
+        String numberGuess;
+        pickNumber = (int) (Math.random() * 100) + 1;
+        Scanner keyboard = new Scanner(System.in);
+        guess = true;
 
         System.out.println("Hello, welcome to the number guessing game. You must find the correct number between 1 and 100.");
 
-            guess = false;
-            pickNumber = (int) (Math.random() * 100) + 1;
-            System.out.println(pickNumber);
-            Scanner keyboard = new Scanner(System.in);
-            do {
-                    //logic
-                    guessedNumber = keyboard.nextInt();
+        System.out.println(pickNumber);
+        guessedNumber= keyboard.nextInt();
 
-                    if (guessedNumber < pickNumber) {
-                        System.out.println("Too low, guess again.");
-                    } else if (guessedNumber > pickNumber) {
-                        System.out.println("Too high, guess again.");
-                    } else {
-                        System.out.println("congratulations!");
-                        guess = true;
-                    }
-                } while (!guess);
+        do {
 
-                //game loop
-               do {
-                   System.out.println("Do you want to play again?");
-                   answer = keyboard.next();
-                   if (answer.equalsIgnoreCase("yes")){
-                       answer = "yes";
-                   }
-                   else(answer = "no")
-               }while();
+           //logic
+            System.out.println(logic());
 
-        }
+            //game loop
+            System.out.println("Do you want to play again?");
+            answer = keyboard.next();
+            if (answer.equalsIgnoreCase("yes")) {
+                answer = "yes";
+            } else {
+                answer = "no";
+            }
+        } while (answer.equals("yes"));
     }
+    //logic method
+    public static String logic(){
+
+        do {
+            if (guessedNumber < pickNumber) {
+                numberGuess = ("Too low, guess again.");
+            }
+            else if (guessedNumber > pickNumber) {
+                numberGuess = ("Too high, guess again.");
+            }
+            else {
+               numberGuess = ("congratulations!");
+                guess = false;
+            }
+        } while (guess);
+        return numberGuess;
+    }
+}
